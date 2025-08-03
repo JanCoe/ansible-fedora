535987d674d52480b25b12c4f737f37fa97ade30
JanCoe <jan@jancoe.io>
2025-08-03 23:27:42 +0100

    path of ansible repo as variable

--- fedora.sh
+++ fedora.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+ansible_dir="$HOME"/Projects/ansible-fedora_xxx
+
 read -s -r -p "Enter your sudo password: " pwd
 echo
 
@@ -9,11 +11,11 @@ printf "==> Installing ansible and git...\n"
 echo "$pwd" | sudo -S dnf install -y ansible git
 
 printf "==> Creating directory for ansible-fedora...\n"
-mkdir -p "$HOME"/Projects/ansible-fedora
+mkdir -p "$ansible_dir"
 
 printf "==> Cloning ansible-fedora...\n"
-git clone https://github.com/JanCoe/ansible-fedora.git "$HOME"/Projects/ansible-fedora
-cd "$HOME"/Projects/ansible-fedora
+git clone https://github.com/JanCoe/ansible-fedora.git "$ansible_dir"
+cd "$ansible_dir"
 
 printf "==> Running playbook...\n"
 echo "$pwd" | sudo ansible-playbook fedora.yml --verbose --inventory localhost
